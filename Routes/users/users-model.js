ce38fcccb4f675fa9bf0b35ce68ccd4c59279972
victor dronov <victordronov1@gmail.com>
2020-10-20 18:25:04 -0400

    added .env

--- Routes/users/users-model.js
+++ Routes/users/users-model.js
@@ -7,20 +7,21 @@ module.exports = {
 
 const db = require("../../data/config");
 
-
 function find() {
-  return db("users_table").select( "id","username").orderBy('id');
+  return db("users_table").select("id", "username").orderBy("id");
 }
 function findById(id) {
-  return db("users_table").where({id});
+  return db("users_table").where({ id });
 }
 function findBy(filter) {
   return db("users_table").where(filter);
 }
 
 function add(user) {
-  return db("users_table").insert(user).then(ids=>{
-    const id = ids[0]
-    return findById(id)
-  });
+  return db("users_table")
+    .insert(user)
+    .then((ids) => {
+      const id = ids[0];
+      return findById(id);
+    });
 }
